f5a65586317a10123d89b137c73e5e4bacdb4f2d
Tobias Schmitt <tschmitt@iup.uni-heidelberg.de>
2022-09-05 14:54:14 +0200

    tests of 2um window

--- firstsimtest.py
+++ firstsimtest.py
@@ -63,6 +63,14 @@ def add_noise_to_spectrum(spectrum, snr, add_noise_window=False):
     return spectrum
 
 
+def down_sample_spectrum(spectrum, spacing):
+    nu_min = spectrum.wavenumber.min().values
+    nu_max = spectrum.wavenumber.max().values
+    new_grid = np.arange(nu_min, nu_max, spacing)
+    spectrum_downsampled = spectrum.interp(wavenumber=new_grid)
+    return spectrum_downsampled
+
+
 def retrieve_spectrum(simulated_measurement):
     result = RetrievalShells.retrieve_measurement(
         measured_spectrum=simulated_measurement,
@@ -79,6 +87,7 @@ def collect_statistic(number_of_runs, snr):
     H2O_results = []
     for n in range(number_of_runs):
         spectrum = noise_free_spectrum()
+        spectrum = down_sample_spectrum(spectrum, 0.24)
         spectrum = add_noise_to_spectrum(spectrum, snr, add_noise_window=True)
         result = retrieve_spectrum(spectrum)
         run_number.append(n)
@@ -94,22 +103,52 @@ def collect_statistic(number_of_runs, snr):
 
 
 # %%
-a = noise_free_spectrum()
-b = add_noise_to_spectrum(a, 200)
-c = add_noise_to_spectrum(a, 1000)
-d = add_noise_to_spectrum(a, 200, add_noise_window=True)
-e = add_noise_to_spectrum(a, 1000, add_noise_window=True)
-# %%
-d_res = retrieve_spectrum(d)
-# %%
-test = collect_statistic(1000, 800)
-# %%
-test.to_netcdf(Path("2u0_snr0800.hdf5"))
+def print_statistics(da):
+    print(f"{da.mean().values} +- {da.std().values}")
+    print(f"{da.std().values / da.mean().values * 100:.2} %")
+
+
 # %%
 snr200 = xr.open_dataset(Path("2u0_snr0200.hdf5"))
 snr800 = xr.open_dataset(Path("2u0_snr0800.hdf5"))
+snr200_downsampled = xr.open_dataset(Path("2u0_snr0200_downsampled.hdf5"))
+snr800_downsampled = xr.open_dataset(Path("2u0_snr0800_downsampled.hdf5"))
 # %%
 px.histogram(x=snr200.CO2)
 # %%
 px.histogram(x=snr800.CO2)
+# %%
+px.histogram(x=snr200.H2O)
+# %%
+px.histogram(x=snr800.H2O)
+# %%
+px.histogram(x=snr200_downsampled.CO2)
+# %%
+px.histogram(x=snr800_downsampled.CO2)
+# %%
+px.histogram(x=snr200_downsampled.H2O)
+# %%
+px.histogram(x=snr800_downsampled.H2O)
+# %%
+# snr200_downsampled = collect_statistic(100, 200)
+# %%
+# snr200_downsampled.to_netcdf(Path("2u0_snr0200_downsampled.hdf5"))
+# %%
+print_statistics(snr200.CO2)
+print_statistics(snr800.CO2)
+print_statistics(snr200.H2O)
+print_statistics(snr800.H2O)
+# %%
+print_statistics(snr200_downsampled.CO2)
+print_statistics(snr800_downsampled.CO2)
+print_statistics(snr200_downsampled.H2O)
+print_statistics(snr800_downsampled.H2O)
+# %%
+snr200_downsampled = collect_statistic(1000, 200)
+# %%
+snr200_downsampled.to_netcdf(Path("2u0_snr0200_downsampled_2.hdf5"))
+# %%
+snr800_downsampled = collect_statistic(1000, 800)
+# %%
+snr800_downsampled.to_netcdf(Path("2u0_snr0800_downsampled_2.hdf5"))
 # %%
